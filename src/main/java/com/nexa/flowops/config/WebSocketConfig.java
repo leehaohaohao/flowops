a13dd07827ace89a7a5cbfd84e6f32e7ffbb56b4
leehao <19817115293@163.com>
2026-04-28 17:40:31 +0800

    fix: 修复 WebSocketConfig 使用 TextWebSocketHandler 替代不存在的 TextMessageHandler

--- src/main/java/com/nexa/flowops/config/WebSocketConfig.java
+++ src/main/java/com/nexa/flowops/config/WebSocketConfig.java
@@ -4,35 +4,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-import com.nexa.flowops.service.LogService;
-import org.springframework.web.socket.handler.TextMessageHandler;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+import org.springframework.web.socket.WebSocketSession;
 
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
 
-    private final LogService logService;
-
-    public WebSocketConfig(LogService logService) {
-        this.logService = logService;
-    }
-
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new TextMessageHandler(), "/ws/logs")
-                .addInterceptors(new org.springframework.web.socket.server.HandshakeInterceptor() {
-                    @Override
-                    public boolean beforeHandshake(org.springframework.http.server.ServerHttpRequest request,
-                                                  org.springframework.web.socket.WebSocketHandler wsHandler,
-                                                  java.util.Map<String, Object> attributes) throws Exception {
-                        return true;
-                    }
-
-                    @Override
-                    public void afterHandshake(org.springframework.http.server.ServerHttpRequest request,
-                                               org.springframework.web.socket.WebSocketHandler wsHandler,
-                                               org.springframework.web.socket.session.WebSocketSession session) {
-                    }
-                });
+        registry.addHandler(new TextWebSocketHandler() {
+            @Override
+            protected void handleTextMessage(WebSocketSession session, org.springframework.web.socket.TextMessage message) {
+            }
+        }, "/ws/logs").setAllowedOrigins("*");
     }
 }
